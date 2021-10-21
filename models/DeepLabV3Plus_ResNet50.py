da519320deebdba6268083f8214099225cf8a63f
hyeonjini <choihj94@gmail.com>
2021-10-21 09:03:15 +0000

    DeepLabV3+ Sample Code push

---
+++ models/DeepLabV3Plus_ResNet50.py
@@ -0,0 +1,16 @@
+from torchvision.models.segmentation.deeplabv3 import DeepLabHead
+from torchvision import models
+import torch.nn as nn
+
+class DeepLabV3Plus_ResNet50(nn.Module):
+    def __init__(self, num_classes=11):
+        super(DeepLabV3Plus_ResNet50, self).__init__()
+
+        self.model = models.segmentation.deeplabv3_resnet50(pretrained=True,
+                                                        progress=True)
+        self.model.classifier = DeepLabHead(2048, num_classes)
+    
+    def forward(self, x):
+        print(self.model)
+        return self.model(x)['out']
+
