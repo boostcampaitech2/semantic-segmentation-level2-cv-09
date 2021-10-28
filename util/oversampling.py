fc0eba8c74e85587ede247650f152eeaaf99cd36
hyeonjini <choihj94@gmail.com>
2021-10-28 05:36:17 +0000

    pseudo labeling

--- util/oversampling.py
+++ util/oversampling.py
@@ -1,6 +1,11 @@
 import argparse
+import json
 import albumentations as A
-
+import tqdm
+import os
+import numpy as np
+import cv2
+from pycocotools.coco import COCO
 
 def apply_augmentation(image, mask):
     transforms = A.Compose([
@@ -16,7 +21,31 @@ def apply_augmentation(image, mask):
 def get_patches():
     pass
 
-def get_background():
+def id_to_objnum(coco, ann):
+    anns = coco.getAnnIds(imgIds=ann['id'])
+    print(anns)
+    return len(coco.getAnnIds(imgIds=ann['id']))
+    
+def get_background(target_categories, num_output, path):
+    """
+        1. json file load
+        2. json file load using coco library
+        3. finding an image have single object
+    """
+    json_file = None
+    with open(path, 'r') as f:
+        json_file = json.load(f)
+
+    coco_from = COCO(path)
+    images = json_file["images"]
+    single_obj_images = []
+    for ann in images:
+        if id_to_objnum(coco_from, ann) == 1:
+            single_obj_images.append(ann)
+    
+    target_single_obj_images = []
+    # for image in single_obj_images:
+
     pass
 
 def do_synthesis(background, patch):
@@ -24,6 +53,7 @@ def do_synthesis(background, patch):
 
 def main(args):
 
+    get_background(None, 0, os.path.join(args.data_dir, args.json_path))
     if args.merge_json:
         print("merge data with original json")
     pass
@@ -35,10 +65,11 @@ if __name__ == "__main__":
     parser.add_argument("--patch")
     parser.add_argument("--background")
     parser.add_argument("--num_output", type=int, default=500)
-    parser.add_argument("--data_dir", type=str, default="opt/ml/segmentation/data/input")
     parser.add_argument("--json_path", type=str, default="train_all.json")
     parser.add_argument("--output_json", type=str, default="oversampled_train.json")
     parser.add_argument("--merge_json", default=True, action='store_true')
+
+    parser.add_argument('--data_dir', type=str, default=os.environ.get('SM_CHANNEL_TRAIN', '/opt/ml/segmentation/input/data/'))
     args = parser.parse_args()
 
     main(args)
---
+++ util/pseudo_labeling.py
@@ -0,0 +1,38 @@
+from dataset import BaseDataset
+import torch
+import os
+import cv2
+import numpy as np
+import pandas as pd
+from shutil import copyfile
+import scipy.ndimage
+
+if __name__ == "__main__":
+    csv_file = "../mmsegmentation/work_dirs/upernet_swin_base_patch4_window12_512x512_160k_ade20k_pretrain_384x384_22K/submission_iter_48000-Copy1.csv"
+    data_dir = "../input/data"
+    output_dir = "../input/data/mmseg/train"
+    target_image_dir = os.path.join(output_dir, "image")
+    target_mask_dir = os.path.join(output_dir, "mask")
+
+    submission = pd.read_csv(csv_file, index_col=None)
+
+    image_ids = submission["image_id"].values
+    masks = submission["PredictionString"].values
+
+    idx = 3272
+    for image_id, mask in zip(image_ids, masks):
+    
+        # image load
+        image = cv2.imread(os.path.join(data_dir, image_id))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        
+        # mask load
+        mask = list(map(int, mask.split()))
+        mask = np.array(mask)
+        mask = np.reshape(mask, (-1, 256))
+        mask = scipy.ndimage.zoom(mask, 2, order=0) # resize 512, 512
+        
+        cv2.imwrite(os.path.join(target_mask_dir, "{0:05d}.png".format(idx)), mask)
+        copyfile(os.path.join(data_dir, image_id), os.path.join(target_image_dir, "{0:05d}.jpg".format(idx)))
+        
+        idx += 1
